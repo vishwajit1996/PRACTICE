db40b861ede1cbeca6fb043f343587aff62c0802
jit12 <jit12@192.168.0.100>
2022-07-24 00:23:32 +0530

    commit

---
+++ MongojavaConnection/src/MongoClass.java
@@ -0,0 +1,17 @@
+
+import com.mongodb.client.*;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.DB;
+
+
+public class MongoClass {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
