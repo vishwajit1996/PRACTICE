842d40c22e3960a5417b305415708a0df22fbc4a
jit12 <jit12@192.168.0.100>
2022-07-24 00:20:44 +0530

    commit

---
+++ filterconfig/src/main/java/HelloServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+	
+		out.print("<br>welcome to servlet<br>");
+		
+	}
+
+}
